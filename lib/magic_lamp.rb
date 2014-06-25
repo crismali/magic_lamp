45c67e734da77e30387798b099e282f9c75e5020
Michael Crismali <michael.crismali@gmail.com>
2014-06-25 08:59:07 -0500

    added MagicLamp#path

--- lib/magic_lamp.rb
+++ lib/magic_lamp.rb
@@ -1,2 +1,12 @@
 module MagicLamp
+  DEFAULT_PATH = "spec/magic_lamp"
+
+  class << self
+    attr_writer :path
+
+    def path
+      path = @path || DEFAULT_PATH
+      Rails.root.join(path)
+    end
+  end
 end
---
+++ spec/lib/magic_lamp_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+describe MagicLamp do
+  context "attr_writer" do
+    it "has path=" do
+      expect(subject).to respond_to(:path=)
+    end
+  end
+
+  describe "#path" do
+    after do
+      subject.path = nil
+    end
+
+    context "spec directory" do
+      let(:spec_path) { Rails.root.join("spec/magic_lamp") }
+
+      it "returns a default path starting from spec" do
+        expect(subject.path).to eq(spec_path)
+      end
+    end
+
+    context "specified" do
+      let(:specified_path) { Rails.root.join("foo") }
+
+      before do
+        subject.path = "foo"
+      end
+
+      it "returns the specified path relative to Rails root" do
+        expect(subject.path).to eq(specified_path)
+      end
+    end
+  end
+end
