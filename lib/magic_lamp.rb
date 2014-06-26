ad52d9c784b2411d2fa88cdef1362026f7739c17
Michael Crismali <michael.crismali@gmail.com>
2014-06-26 11:48:59 -0500

    added pass through create fixture method to MagicLamp

--- lib/magic_lamp.rb
+++ lib/magic_lamp.rb
@@ -1,4 +1,9 @@
 module MagicLamp
+  class << self
+    def create_fixture(fixture_name, controller_class, &block)
+      FixtureCreator.new.create_fixture(fixture_name, controller_class, &block)
+    end
+  end
 end
 
 require "magic_lamp/fixture_creator"
---
+++ spec/lib/magic_lamp_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe MagicLamp do
+
+  describe "#create_fixture" do
+
+    it "passes through its arguments and block to an instance of FixtureCreator" do
+      passed_block = Proc.new { render :foo }
+      fixture_creator = MagicLamp::FixtureCreator.new
+      allow(MagicLamp::FixtureCreator).to receive(:new).and_return(fixture_creator)
+
+      expect(fixture_creator).to receive(:create_fixture) do |fixture_name, controller_class, &block|
+        expect(fixture_name).to eq("foo")
+        expect(controller_class).to eq(OrdersController)
+        expect(block).to eq(passed_block)
+      end
+
+      subject.create_fixture("foo", OrdersController, &passed_block)
+    end
+  end
+end
