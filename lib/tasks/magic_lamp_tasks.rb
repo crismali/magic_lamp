522ce3d3686eba502be93c2668e780af7ebf2576
Michael Crismali <michael.crismali@gmail.com>
2014-07-05 10:09:38 -0500

    added convenience tasks for a few different js/rails test runners

--- lib/tasks/magic_lamp_tasks.rb
+++ lib/tasks/magic_lamp_tasks.rb
@@ -10,7 +10,30 @@ namespace :magic_lamp do
   task create_fixtures: :clean do
     MagicLamp.load_lamp_files
   end
+
+  # Test runner integration tasks
+
+  desc "Create fixtures and run Teaspoon specs"
+  task teaspoon: :create_fixtures do
+    Rake::Task["teaspoon"].invoke
+  end
+
+  desc "Create fixtures and run Jasmine Rails specs"
+  task jasmine: :create_fixtures do
+    Rake::Task["spec:javascript"].invoke
+  end
+
+  desc "Create fixtures and run Evergreen specs"
+  task evergreen: :create_fixtures do
+    Rake::Task["spec:javascripts"].invoke
+  end
+
+  desc "Create fixtures and run Konacha specs"
+  task konacha: :create_fixtures do
+    Rake::Task["konacha:run"].invoke
+  end
 end
 
+# Alias for magic_lamp:create_fixtures
 desc "Creates fixtures from MagicLamp files"
 task magic_lamp: "magic_lamp:create_fixtures"
