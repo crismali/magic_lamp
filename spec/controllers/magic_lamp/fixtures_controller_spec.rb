19a2bf0c90dbd9c45072f9ff3549c6ef952c1d7c
Michael Crismali <michael.crismali@gmail.com>
2014-08-02 19:05:06 -0500

    improved spec; added bootstrap script since sqlite gets confused

--- spec/controllers/magic_lamp/fixtures_controller_spec.rb
+++ spec/controllers/magic_lamp/fixtures_controller_spec.rb
@@ -13,11 +13,13 @@ module MagicLamp
       let(:parsed_response) { JSON.parse(response.body) }
       let(:foo_fixture) { parsed_response["orders/foo"] }
       let(:bar_fixture) { parsed_response["orders/bar"] }
+      let(:form_fixture) { parsed_response["orders/form"] }
 
       it "returns a json hash of all registered fixtures" do
         get :index, use_route: :magic_lamp
         expect(foo_fixture).to eq("foo\n")
         expect(bar_fixture).to eq("bar\n")
+        expect(form_fixture).to match(/<div class="actions"/)
       end
     end
   end
--- spec/dummy/spec/magical/magic_lamp.rb
+++ spec/dummy/spec/magical/magic_lamp.rb
@@ -1,9 +1,12 @@
-require "ostruct"
-
 MagicLamp.register_fixture do
   render "orders/foo"
 end
 
 MagicLamp.register_fixture do
   render "orders/bar"
+end
+
+MagicLamp.register_fixture do
+  @order = Order.new
+  render partial: "orders/form"
 end
