6217e4c35933ce92c70c2724c9d16b47780dd947
Michael Crismali <michael.crismali@gmail.com>
2014-08-05 08:45:12 -0500

    gave genie class a new prototype rather than appending to the existing one

--- app/assets/javascripts/magic_lamp/genie.js
+++ app/assets/javascripts/magic_lamp/genie.js
@@ -4,14 +4,17 @@
     this.cache = {};
   }
 
-  Genie.prototype.createFixtureContainer = function() {
-    var div = document.createElement('div');
-    div.setAttribute('id', 'magic-lamp');
-    this.fixtureContainer = div;
-  };
+  Genie.prototype = {
 
-  Genie.prototype.appendFixtureContainer = function() {
-    document.body.appendChild(this.fixtureContainer);
+    createFixtureContainer: function() {
+      var div = document.createElement('div');
+      div.setAttribute('id', 'magic-lamp');
+      this.fixtureContainer = div;
+    },
+
+    appendFixtureContainer: function() {
+      document.body.appendChild(this.fixtureContainer);
+    }
   };
 
   MagicLamp.Genie = Genie;
