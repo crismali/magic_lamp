f33dddda9b8f65c4405d198e11fb5190100ece12
Michael Crismali <michael.crismali@gmail.com>
2014-08-10 19:09:01 -0500

    broke out magic lamp js so application js can be in charge of initialization

--- app/assets/javascripts/magic_lamp/application.js
+++ app/assets/javascripts/magic_lamp/application.js
@@ -10,6 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require_self
+//= require magic_lamp/magic_lamp
 //= require magic_lamp/genie
-var MagicLamp = {};
+//= require_self
