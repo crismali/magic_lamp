6e9e0f387f464a798af93939ac977b5ebc2b8aa7
Michael Crismali <michael.crismali@gmail.com>
2014-08-17 12:52:28 -0500

    got rid of redundant spec context

--- spec/controllers/magic_lamp/fixtures_controller_spec.rb
+++ spec/controllers/magic_lamp/fixtures_controller_spec.rb
@@ -2,52 +2,50 @@ require "rails_helper"
 
 module MagicLamp
   describe FixturesController do
-    context "errors" do
-      shared_examples "it handles errors" do
-        before do
-          allow(MagicLamp).to receive(:generate_fixture).and_raise(error, "message")
-        end
-
-        it "outputs the error and the message to the server log" do
-          expect(controller.logger).to receive(:error) do |error_message|
-            expect(error_message).to match(/message\n\s\s\s\s.*magic_lamp\//m)
-          end
-          get :show, name: "orders/baz", use_route: :magic_lamp
-        end
-
-        it "renders the error as text for the client side" do
-          get :show, name: "orders/baz", use_route: :magic_lamp
-          expect(response.body).to eq("message")
-        end
-
-        it "renders a 500" do
-          get :show, name: "orders/baz", use_route: :magic_lamp
-          expect(response.status).to eq(500)
-        end
+    shared_examples "it handles errors" do
+      before do
+        allow(MagicLamp).to receive(:generate_fixture).and_raise(error, "message")
       end
 
-      context "ArgumentError" do
-        it_behaves_like "it handles errors" do
-          let(:error) { ArgumentError }
+      it "outputs the error and the message to the server log" do
+        expect(controller.logger).to receive(:error) do |error_message|
+          expect(error_message).to match(/message\n\s\s\s\s.*magic_lamp\//m)
         end
+        get :show, name: "orders/baz", use_route: :magic_lamp
       end
 
-      context "AlreadyRegisteredFixtureError" do
-        it_behaves_like "it handles errors" do
-          let(:error) { AlreadyRegisteredFixtureError }
-        end
+      it "renders the error as text for the client side" do
+        get :show, name: "orders/baz", use_route: :magic_lamp
+        expect(response.body).to eq("message")
       end
 
-      context "AmbiguousFixtureNameError" do
-        it_behaves_like "it handles errors" do
-          let(:error) { AmbiguousFixtureNameError }
-        end
+      it "renders a 500" do
+        get :show, name: "orders/baz", use_route: :magic_lamp
+        expect(response.status).to eq(500)
       end
+    end
 
-      context "UnregisteredFixtureError" do
-        it_behaves_like "it handles errors" do
-          let(:error) { UnregisteredFixtureError }
-        end
+    context "ArgumentError" do
+      it_behaves_like "it handles errors" do
+        let(:error) { ArgumentError }
+      end
+    end
+
+    context "AlreadyRegisteredFixtureError" do
+      it_behaves_like "it handles errors" do
+        let(:error) { AlreadyRegisteredFixtureError }
+      end
+    end
+
+    context "AmbiguousFixtureNameError" do
+      it_behaves_like "it handles errors" do
+        let(:error) { AmbiguousFixtureNameError }
+      end
+    end
+
+    context "UnregisteredFixtureError" do
+      it_behaves_like "it handles errors" do
+        let(:error) { UnregisteredFixtureError }
       end
     end
 
