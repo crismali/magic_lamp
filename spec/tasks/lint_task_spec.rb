eb86bb05d03e82a2bf14662434ead9b06434fca7
Michael Crismali <michael.crismali@gmail.com>
2014-09-04 22:12:02 -0500

    refactored spec to prevent noise in test output

--- spec/tasks/lint_task_spec.rb
+++ spec/tasks/lint_task_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 describe "Lint task" do
   it "generates all fixtures" do
     expect(MagicLamp).to receive(:generate_all_fixtures)
-    Rake::Task["magic_lamp:lint"].invoke
+    dummy = double(puts: nil)
+    task_proc = Rake::Task["magic_lamp:lint"].actions.first
+    dummy.instance_eval(&task_proc)
   end
 end
