a679a437b1c8b3cf1a19fb2fbecb4662fdb7a9b6
Michael Crismali <michael.crismali@gmail.com>
2014-12-27 20:04:50 -0600

    added magic_lamp:lint:config task

--- lib/tasks/lint_task.rb
+++ lib/tasks/lint_task.rb
@@ -1,14 +1,40 @@
 require "method_source"
 
 namespace :magic_lamp do
-  desc "Generates all Magic Lamp fixtures for debugging"
+  desc "Generates all Magic Lamp fixtures and displays errors for debugging"
   task lint: :environment do
     file_errors, fixture_errors = MagicLamp.lint_fixtures.values_at(:files, :fixtures)
     template = File.read("#{File.dirname(__FILE__)}/lint_template.erb")
     erb_template = ERB.new(template)
     puts erb_template.result(binding)
   end
+
+  namespace :lint do
+
+    desc "Lints your Magic Lamp configuration and reports any errors"
+    task config: :environment do
+      puts "Linting Magic Lamp configuration..."
+      errors = MagicLamp.lint_config
+      if errors.present?
+        if errors[:config_file_load].present?
+          puts "Loading configuration failed with:"
+          puts "\n#{errors[:config_file_load]}"
+        end
+
+        [:before_each, :after_each].each do |callback|
+          if errors[callback].present?
+            puts "Executing #{callback} failed with:"
+            puts "\n#{errors[callback]}"
+          end
+        end
+
+      else
+        puts "Configuration looks good!"
+      end
+    end
+
+  end
 end
 
-desc "Generates all Magic Lamp fixtures for debugging (alias for `magic_lamp:lint`)"
+desc "Generates all Magic Lamp fixtures and displays errors for debugging (alias for `magic_lamp:lint`)"
 task mll: "magic_lamp:lint"
--- spec/tasks/lint_task_spec.rb
+++ spec/tasks/lint_task_spec.rb
@@ -1,5 +1,18 @@
 require "rails_helper"
 
+module CatchOutput
+  def capture_stdout
+    begin
+      old_stdout = $stdout
+      $stdout = StringIO.new("", "w")
+      yield
+      $stdout.string
+    ensure
+      $stdout = old_stdout
+    end
+  end
+end
+
 describe "magic_lamp:lint" do
   let!(:output) { [] }
   let(:outputs) { output.join("\n") }
@@ -55,6 +68,45 @@ describe "magic_lamp:lint" do
       expect(outputs).to match(/RuntimeError: second fixture/i)
       expect(outputs).to match(/extensions:/i)
       expect(outputs).to match(/controller: applicationcontroller/i)
+    end
+  end
+end
+
+describe "magic_lamp:lint:config" do
+  include CatchOutput
+  let(:output) { capture_stdout { subject.execute } }
+
+  it { is_expected.to depend_on(:environment) }
+
+  context "no errors" do
+    it "tells us that everything looks good" do
+      expect(output).to match("Linting Magic Lamp configuration")
+      expect(output).to match("Configuration looks good")
+    end
+  end
+
+  context "errors" do
+    it "tells us if the file could be loaded" do
+      allow(MagicLamp).to receive(:load_config) do
+        load Rails.root.join("error_specs", "config_file_load_error.rb")
+      end
+      expect(output).to match("Linting Magic Lamp configuration")
+      expect(output).to_not match("Configuration looks good")
+      expect(output).to match("Loading configuration failed")
+      expect(output).to match("RuntimeError: Nope")
+    end
+
+    [:before, :after].each do |callback_type|
+      it "tells us if the #{callback_type}_each callback is broken" do
+        MagicLamp.configuration.send("#{callback_type}_each") do
+          raise "Nope"
+        end
+
+        expect(output).to match("Linting Magic Lamp configuration")
+        expect(output).to_not match("Configuration looks good")
+        expect(output).to match("Executing #{callback_type}_each failed with:")
+        expect(output).to match("RuntimeError: Nope")
+      end
     end
   end
 end
